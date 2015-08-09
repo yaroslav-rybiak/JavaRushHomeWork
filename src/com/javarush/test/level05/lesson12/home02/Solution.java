fc87de4e15eaf0c27a9aac9de4d99947865f225d
Yaroslav Rybyak <yaroslav.rybiak@gmail.com>
2015-08-09 15:13:57 +0300

    level 05 lesson 12 home 01-02

---
+++ src/com/javarush/test/level05/lesson12/home02/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level05.lesson12.home02;
+
+/* Man and Woman
+1. Внутри класса Solution создай public static классы Man и Woman.
+2. У классов должны быть поля: name(String), age(int), address(String).
+3. Создай конструкторы, в которые передаются все возможные параметры.
+4. Создай по два объекта каждого класса со всеми данными используя конструктор.
+5. Объекты выведи на экран в таком формате [name + " " + age + " " + address].
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //создай по два объекта каждого класса тут
+        Man man1 = new Man("1", 1, "1");
+        Man man2 = new Man("1", 1, "1");
+        Woman man3 = new Woman("1", 1, "1");
+        Woman man4 = new Woman("1", 1, "1");
+
+        //выведи их на экран тут
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        System.out.println(man3.name + " " + man3.age + " " + man3.address);
+        System.out.println(man4.name + " " + man4.age + " " + man4.address);
+    }
+
+    //добавьте тут ваши классы
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
