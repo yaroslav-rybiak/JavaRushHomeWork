f0b410db0b1bae87cb63763066658e52cda41dff
Yaroslav Rybyak <yaroslav.rybiak@gmail.com>
2015-08-10 10:21:27 +0300

    level 06 lesson 05

---
+++ src/com/javarush/test/level06/lesson05/task03/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level06.lesson05.task03;
+
+/* По 50 000 объектов Cat и Dog
+Создать в цикле по 50 000 объектов Cat и Dog. (Java-машина должна начать уничтожать неиспользуемые, и метод finalize хоть раз да вызовется).
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        Cat[] catArray = new Cat[50000];
+        Dog[] dogArray = new Dog[50000];
+        for (int i = 0; i < 50000; i++) {
+            catArray[i] = new Cat();
+            dogArray[i] = new Dog();
+        }
+
+    }
+}
+class Cat
+{
+    @Override
+    protected void finalize() throws Throwable
+    {
+        super.finalize();
+        System.out.println("Cat destroyed");
+    }
+}
+
+class Dog
+{
+    @Override
+    protected void finalize() throws Throwable
+    {
+        super.finalize();
+        System.out.println("Dog destroyed");
+    }
+}
