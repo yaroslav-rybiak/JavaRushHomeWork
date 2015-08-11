93bfa3663b090881f76f12e4bd0f512ab0b7621d
Yaroslav Rybyak <yaroslav.rybiak@gmail.com>
2015-08-11 10:23:33 +0300

    level 06 lesson 11 home 03-07

---
+++ src/com/javarush/test/level06/lesson11/home06/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level06.lesson11.home06;
+
+/* KissMyShinyMetalAss
+Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        KissMyShinyMetalAss lol = new KissMyShinyMetalAss();
+        System.out.println(lol);
+    }
+
+    public static class KissMyShinyMetalAss
+    {
+
+    }
+
+}
