a395395879ebf2fa4a787b0b81ae68ee4cfc0b43
Yaroslav Rybyak <yaroslav.rybiak@gmail.com>
2015-08-12 23:33:38 +0300

    level 08 lesson 06

---
+++ src/com/javarush/test/level08/lesson06/task05/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level08.lesson06.task05;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* Четыре метода
+Реализовать 4 метода. Они должны возвращать список, который лучше всего подходит для выполнения данных операций (быстрее всего справится с большим количеством операций). Ничего измерять не нужно.
+*/
+
+public class Solution
+{
+    public static List  getListForGet()
+    {
+        //напишите тут ваш код
+        ArrayList arrayList = new ArrayList();
+        return arrayList;
+    }
+
+    public static List  getListForSet()
+    {
+        //напишите тут ваш код
+        ArrayList arrayList = new ArrayList();
+        return arrayList;
+    }
+
+    public static List  getListForAddOrInsert()
+    {
+        //напишите тут ваш код
+        LinkedList linkedList = new LinkedList();
+        return linkedList;
+
+    }
+
+    public static List  getListForRemove()
+    {
+        //напишите тут ваш код
+        LinkedList linkedList = new LinkedList();
+        return linkedList;
+    }
+}
